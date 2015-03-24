c61755c071eff39d7ac34b0d3ded1b740ce8ac76
Scott Barkman <scott@liftinteractive.com>
2015-03-24 10:03:22 -0600

    update stored_messages with admin tweaks

--- stored_messages/admin.py
+++ stored_messages/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from .models import Inbox, Message, MessageArchive
 
+# todo: raw ID fields.
 
 admin.site.register(Inbox)
 admin.site.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    '''
+        Admin View for Message
+    '''
+    raw_id_fields = ('sender', 'thread',)
+    readonly_fields = ('',)
+    search_fields = ['sender']
+
+admin.site.register(Message, MessageAdmin)
 admin.site.register(MessageArchive)
